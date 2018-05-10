473d57333af52a1d28abaa9362bd1343f9762c74
rstar2 <rstar2@abv.bg>
2018-05-10 15:02:08 +0300

    more AWS-lamda

--- src/aws-lambda-expirations-check/handler.js
+++ src/aws-lambda-expirations-check/handler.js
@@ -7,70 +7,85 @@ const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const TWILIO_SENDER = process.env.TWILIO_SENDER;
 
-module.exports.check = (event, context, callback) => {
-  console.time("Invoking function check took");
+module.exports.check = async (event, context, callback) => {
+	console.time("Invoking function check took");
 
-  const now = moment(Date.now()).format("MMM Do YY");;
+	const now = moment(Date.now()).format("MMM Do YY");
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Checked on ${now} - SMS sender ${TWILIO_SENDER}`,
-      event,
-    }),
-  };
+	await dbCheck();
 
-  console.timeEnd("Invoking function check took");
-  callback(null, response);
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: `Checked on ${now}`,
+			event,
+		}),
+	};
+
+	console.timeEnd("Invoking function check took");
+	callback(null, response);
 };
 
 
-module.exports.api = (event, context, callback) => {
-  console.time("Invoking function api took");
-  switch (event.httpMethod) {
-    case 'GET':
-      break;
-    case 'POST':
-      break;
-    default:
-      return callback(`Unsupported API gateway with HTTP method ${event.httpMethod}`);
-  }
+module.exports.api = async (event, context, callback) => {
+	console.time("Invoking function api took");
 
-  let name = "Anonymous";
+	let responseBody;
+	switch (event.httpMethod) {
+		case 'GET':
+			switch (event.path) {
+				case '/list':
+					responseBody = await dbList();
+					break;
+				default:
+					return callback(`Unsupported API gateway with HTTP GET path ${event.path}`);
+			}
+			break;
 
-  if (event.queryStringParameters && event.queryStringParameters.name) {
-    // extract parameters from the request's query
-    name = event.queryStringParameters.name;
-  } else if (event.httpMethod === 'POST') {
-    // extract parameters from HTTP POST request
+		// eslint-disable-next-line no-case-declarations
+		case 'POST':
+			// assume it's JSON ("application/json")
+			const json = JSON.parse(event.body);
+			switch (event.path) {
+				case '/add':
+					responseBody = await dbAdd(json);
+					break;
+				case '/delete':
+					responseBody = await dbRemove(json);
+					break;
+				default:
+					return callback(`Unsupported API gateway with HTTP POST path ${event.path}`);
+			}
+			break;
+		default:
+			return callback(`Unsupported API gateway with HTTP method ${event.httpMethod}`);
+	}
 
-    // assume it's JSON ("application/json")
-    const json = JSON.parse(event.body);
-    if (json.name) {
-      name = json.name;
-    }
-  }
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify(responseBody),
+	};
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello ${name}`,
-      event,
-    }),
-  };
-
-  console.time("Invoking function api took");
-  callback(null, response);
+	console.time("Invoking function api took");
+	callback(null, response);
 };
 
-const apiList = () => {
+const dbCheck = async () => {
 
 };
 
-const apiAdd = () => {
+const dbList = async () => {
+	return new Promise((resolve, reject) =>
+		setTimeout(() => {
+			resolve({id1: "1111", id2: "2222",});
+		}, 1000)
+	);
+};
+
+const dbAdd = async (json) => {
 
 };
 
-const apiRemove = () => {
+const dbRemove = async (json) => {
 
 };
