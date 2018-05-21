e96e07221995174802bd3ddfd857aca1eeb5ae03
rstar2 <rstar2@abv.bg>
2018-05-21 11:55:55 +0300

    asd

---
+++ src/web-push/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const webpush = require('web-push');
+
+const app = express();
+
+app.use(require('body-parser').json());
+
+app.post('/subscribe', (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({ title: 'test' });
+
+  console.log(subscription);
+
+  webpush.sendNotification(subscription, payload).catch(error => {
+    console.error(error.stack);
+  });
+});
