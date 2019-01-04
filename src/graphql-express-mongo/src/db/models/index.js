50c17fb0adeac19a8884c95ebafbf38beb1bc86f
rstar2 <rstar2@abv.bg>
2019-01-04 18:03:10 +0200

    asd

--- src/graphql-express-mongo/src/db/models/index.js
+++
@@ -1,7 +0,0 @@
-const Event = require('./event');
-const User = require('./user');
-const Booking = require('./booking');
-
-module.exports = {
-    Event, User, Booking
-};
--- src/graphql-express-mongo/src/graphql/index.js
+++
@@ -1,8 +0,0 @@
-const graphqlHTTP = require('express-graphql');
-
-module.exports = graphqlHTTP({
-    schema: require('./schema'),
-    // root schema-resolvers
-    rootValue: require('./resolvers'),
-    graphiql: true,
-});
